77d48d741b681d1da8d5b82aa86d58b9cb8023e1
Phat G Ha <phatg@lecle.co.kr>
2017-10-14 03:37:16 +0700

    Add dot border

--- scatter.jsx
+++ scatter.jsx
@@ -4,19 +4,32 @@ var jsonist = require('jsonist')
 var createReactClass = require('create-react-class')
 
 var Dot = createReactClass({
+  getInitialState: function() {
+    return { isHovered: false };
+  },
+
   handleMouseEnter: function() {
     var statsData = this.props.data;
     this.props.showStats(statsData);
+    this.setState({ isHovered: true });
   },
 
   handleMouseLeave: function() {
     this.props.hideStats();
+    this.setState({ isHovered: false });
   },
 
   render() {
+    var dotStyles = this.props.style;
+    if(this.state.isHovered) {
+      dotStyles['border'] = "1px solid #fff";
+    } else {
+      dotStyles['border'] = "none";
+    }
+
     return (
       <div className="dot" 
-            style={this.props.style} 
+            style={dotStyles} 
             onMouseEnter={this.handleMouseEnter}
             onMouseLeave={this.handleMouseLeave}
       />
